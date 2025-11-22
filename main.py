9b13e18a57ea2a85173cb0ab2a32211203ad879b
asif-kamal <asif.kamal1489@gmail.com>
2025-11-21 19:11:10 -0600

    built turtle race - completed finding the winner

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import random
 from turtle import Turtle, Screen
+from tkinter import messagebox
 
 screen = Screen()
-
-screen.setup(width=500, height=400)
+is_race_on = False
+screen.setup(width=500, height=600)
 choice = screen.textinput(title="choose_turtle", prompt="Enter the color of the turtle you want to win: ")
 
-colors = ["red", "green", "blue", "yellow", "purple", "pink", "white"]
+if choice:
+    is_race_on = True
+
+colors = ["red", "green", "blue", "yellow", "purple", "pink", "black"]
 racing_turtles = {}
 y = -160
 
@@ -15,5 +20,19 @@ for num in range(len(colors)):
     racing_turtles[f"t{num}"].penup()
     racing_turtles[f"t{num}"].goto(x=-230,y=y)
     y += 65
+
+while is_race_on:
+
+    for turtle in racing_turtles.values():
+        if turtle.xcor() < 222:
+            random_distance = random.randint(0, 10)
+            turtle.forward(random_distance)
+        else:
+            is_race_on = False
+            messagebox.showinfo("result", f"The winning color of the turtle is {turtle.fillcolor()}")
+            if choice == turtle.fillcolor():
+                messagebox.showinfo("Winner", "You won!")
+            else:
+                messagebox.showinfo("Lost", "You lost!")
 
 screen.exitonclick()
